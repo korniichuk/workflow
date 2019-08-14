c7f1ef4dcf72ea83cebb8370252fb20ae5baad76
Ruslan Korniichuk <ruslan.korniichuk@gmail.com>
2019-08-14 17:26:10 +0200

    [workflow][aws] Preprocess JSON and save to S3 as CSV file

--- workflow-aws/lambda/workflow-preprocess-json/workflow_preprocess_json.py
+++ workflow-aws/lambda/workflow-preprocess-json/workflow_preprocess_json.py
@@ -1,18 +1,46 @@
 # Name: workflow-preprocess-json
-# Version: 0.1a1
+# Version: 0.1a2
 
+import gzip
+from io import StringIO
 import json
+import os
+
+import boto3
+import pandas as pd
+import s3fs
 
 
 def lambda_handler(event, context):
 
+    src_bucket_name = 'korniichuk.demo'
+    src_key = 'workflow/input/11.gz'
+    src = os.path.join(src_bucket_name, src_key)
     result = {}
-
     try:
-        pass
+        fs = s3fs.S3FileSystem(anon=False)
+        with fs.open(src, 'rb') as f:
+            g = gzip.GzipFile(fileobj=f)
+            df = pd.read_json(g, lines=True, convert_dates=['date'])
     except BaseException as e:
         print(e)
         raise e
+    df = df[['date', 'gross', 'net', 'tax', 'email']]
+    buff = StringIO()
+    df.to_csv(buff, index=False)
+    dst_bucket_name = 'korniichuk.tmp'
+    dst_key = 'workflow-orders/csv/11.csv'
+    try:
+        s3 = boto3.resource('s3')
+        s3.Object(dst_bucket_name, dst_key).put(Body=buff.getvalue())
+    except BaseException as e:
+        print(e)
+        raise e
+    else:
+        dst = os.path.join(dst_bucket_name, dst_key)
+        msg = 'Preprocessed data saved to {} file'.format(dst)
+        print(msg)
+        result['dst'] = dst
     return {
         'statusCode': 200,
         'body': json.dumps(result)
