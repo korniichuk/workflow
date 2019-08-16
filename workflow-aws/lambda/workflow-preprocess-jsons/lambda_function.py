adfbb855d9a1d02cb93ea55824192143a7a9dd84
Ruslan Korniichuk <ruslan.korniichuk@gmail.com>
2019-08-16 17:19:00 +0200

    [workflow][aws] workflow-preprocess-jsons sends 's3://*' paths to workflow-preprocess-json

--- workflow-aws/lambda/workflow-preprocess-jsons/lambda_function.py
+++ workflow-aws/lambda/workflow-preprocess-jsons/lambda_function.py
@@ -1,5 +1,5 @@
 # Name: workflow-preprocess-jsons
-# Version: 0.1a2
+# Version: 0.1a3
 
 import json
 import os
@@ -8,24 +8,22 @@ import boto3
 
 
 def lambda_handler(event, context):
-
     bucket_name = 'korniichuk.demo'
     lmbd_name = 'workflow-preprocess-json'
-    result = {'keys': []}
-
+    result = {'paths': []}
     s3 = boto3.resource('s3')
     lmbd = boto3.client('lambda')
     for obj in s3.Bucket(bucket_name).objects.all():
         key = obj.key
         if key.startswith('workflow/input/') and key.endswith('.gz'):
-            result['keys'].append(key)
-            src = os.path.join(bucket_name, key)
+            src = 's3://' + os.path.join(bucket_name, key)
+            result['paths'].append(src)
             data = {'src': src}
             payload = json.dumps(data)
             lmbd.invoke(FunctionName=lmbd_name,
                         InvocationType='Event',
                         Payload=payload)
-    result['num'] = len(result['keys'])
+    result['num'] = len(result['paths'])
     return {
         'statusCode': 200,
         'body': json.dumps(result)
