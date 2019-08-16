e67e310439d27d5544106e4afbe5a27a955a513d
Ruslan Korniichuk <ruslan.korniichuk@gmail.com>
2019-08-16 20:15:25 +0200

    [workflow][aws] workflow-cacl-orders with Targer concept

--- workflow-aws/lambda/workflow-calc-orders/lambda_function.py
+++ workflow-aws/lambda/workflow-calc-orders/lambda_function.py
@@ -1,5 +1,5 @@
 # Name: workflow-calc-orders
-# Version: 0.1a1
+# Version: 0.1a2
 
 from io import StringIO
 import json
@@ -7,13 +7,41 @@ import os
 
 import arrow
 import boto3
+import botocore
 import pandas as pd
 
 
+def exists_in_s3(bucket_name, key):
+    """Does object exist in S3 bucket"""
+
+    s3 = boto3.resource('s3')
+    try:
+        s3.Object(bucket_name, key).load()
+    except botocore.exceptions.ClientError as e:
+        if e.response['Error']['Code'] == '404':
+            return False
+        else:
+            msg = 'Error: {} object checking in {} S3 bucket is failed'.format(
+                    key, bucket_name)
+            print(msg)
+            return False
+    else:
+        return True
+
+
 def lambda_handler(event, context):
-
     result = {}
-
+    dst_bucket_name = 'korniichuk.demo'
+    date = arrow.utcnow().format('YYYYMMDD')
+    dst_filename = 'orders_{}.csv'.format(date)
+    dst_key = 'workflow/output/{}'.format(dst_filename)
+    dst = 's3://' + os.path.join(dst_bucket_name, dst_key)
+    result['dst'] = dst
+    if exists_in_s3(dst_bucket_name, dst_key):
+        return {
+            'statusCode': 200,
+            'body': json.dumps(result)
+        }
     src = event['src']
     tmp = pd.read_csv(src, parse_dates=['date'])
     tmp = tmp.groupby('email').count().sort_values('email')
@@ -22,21 +50,10 @@ def lambda_handler(event, context):
     df['orders'] = tmp.date.values
     buff = StringIO()
     df.to_csv(buff, index=False)
-    dst_bucket_name = 'korniichuk.demo'
-    date = arrow.utcnow().format('YYYYMMDD')
-    dst_filename = 'orders_{}.csv'.format(date)
-    dst_key = 'workflow/output/{}'.format(dst_filename)
-    try:
-        s3 = boto3.resource('s3')
-        s3.Object(dst_bucket_name, dst_key).put(Body=buff.getvalue())
-    except BaseException as e:
-        print(e)
-        raise e
-    else:
-        dst = 's3://' + os.path.join(dst_bucket_name, dst_key)
-        msg = 'Orders saved to {} file'.format(dst)
-        print(msg)
-        result['dst'] = dst
+    s3 = boto3.resource('s3')
+    s3.Object(dst_bucket_name, dst_key).put(Body=buff.getvalue())
+    msg = 'Orders saved to {} file'.format(dst)
+    print(msg)
     return {
         'statusCode': 200,
         'body': json.dumps(result)
