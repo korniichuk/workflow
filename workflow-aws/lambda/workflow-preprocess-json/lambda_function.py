d38584d4a5da09069f70c4c0667a2a68551cf7ca
Ruslan Korniichuk <ruslan.korniichuk@gmail.com>
2019-08-19 18:22:45 +0200

    [workflow][aws] workflow-preprocess-json works w/o s3fs lib

--- workflow-aws/lambda/workflow-preprocess-json/lambda_function.py
+++ workflow-aws/lambda/workflow-preprocess-json/lambda_function.py
@@ -1,29 +1,28 @@
 # Name: workflow-preprocess-json
-# Version: 0.1a5
+# Version: 0.1a6
 
 import gzip
-from io import StringIO
+from io import BytesIO, StringIO
 import json
 import os
 
 import boto3
 import botocore
 import pandas as pd
-import s3fs
 
 
-def exists_in_s3(bucket_name, key):
+def exists_in_s3(bucket, key):
     """Does object exist in S3 bucket"""
 
     s3 = boto3.resource('s3')
     try:
-        s3.Object(bucket_name, key).load()
+        s3.Object(bucket, key).load()
     except botocore.exceptions.ClientError as e:
         if e.response['Error']['Code'] == '404':
             return False
         else:
             msg = 'Error: {} object checking in {} S3 bucket is failed'.format(
-                    key, bucket_name)
+                    key, bucket)
             print(msg)
             return False
     else:
@@ -35,24 +34,25 @@ def lambda_handler(event, context):
     src = event['src']
     src_filename = os.path.basename(src)
     dst_filename = src_filename.replace('.gz', '.csv')
-    dst_bucket_name = 'korniichuk.tmp'
+    dst_bucket = 'korniichuk.tmp'
     dst_key = 'workflow-orders/csv/{}'.format(dst_filename)
-    dst = 's3://' + os.path.join(dst_bucket_name, dst_key)
+    dst = 's3://' + os.path.join(dst_bucket, dst_key)
     result['dst'] = dst
-    if exists_in_s3(dst_bucket_name, dst_key):
+    if exists_in_s3(dst_bucket, dst_key):
         return {
             'statusCode': 200,
             'body': json.dumps(result)
         }
-    fs = s3fs.S3FileSystem(anon=False, default_fill_cache=False)
-    with fs.open(src, 'rb') as f:
-        g = gzip.GzipFile(fileobj=f)
-        df = pd.read_json(g, lines=True, convert_dates=['date'])
+    s3 = boto3.resource('s3')
+    src_bucket = src.replace('s3://', '').split('/')[0]
+    src_key = src.replace('s3://', '')[len(src_bucket)+1:]
+    f = BytesIO(s3.Object(src_bucket, src_key).get()['Body'].read())
+    g = gzip.GzipFile(fileobj=f)
+    df = pd.read_json(g, lines=True, convert_dates=['date'])
     df = df[['date', 'gross', 'net', 'tax', 'email']]
     buff = StringIO()
     df.to_csv(buff, index=False)
-    s3 = boto3.resource('s3')
-    s3.Object(dst_bucket_name, dst_key).put(Body=buff.getvalue())
+    s3.Object(dst_bucket, dst_key).put(Body=buff.getvalue())
     msg = 'Preprocessed data saved to {} file'.format(dst)
     print(msg)
     return {
